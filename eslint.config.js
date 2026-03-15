378fc8f515ca10019d49644c348ca13711163b57
za522 <116058758+za522@users.noreply.github.com>
2026-03-15 16:04:55 +0000

    Delete eslint.config.js

--- eslint.config.js
+++
@@ -1,1 +0,0 @@
-export default { env: { browser: true, es2021: true }, extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"], parserOptions: { ecmaVersion: 12, sourceType: "module", ecmaFeatures: { jsx: true } }, plugins: ["react", "react-hooks"], rules: { "react/prop-types": "off", "react/react-in-jsx-scope": "off" } };
